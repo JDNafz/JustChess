262a274c9cf1057afece0e47f99d59b2aa7cd8de
JDNafz <nafziger10@gmail.com>
2023-09-27 00:23:52 -0500

    removed server console.log

--- server/routes/board.router.js
+++ server/routes/board.router.js
@@ -13,10 +13,7 @@ router.get("/", (req, res) => {
   pool
     .query(sqlText)
     .then((result) => {
-      // THIS WAS OLD SERVER SIDE MOVEMENTS
-      // const stateObj = checkState( result.rows );
-      // res.send(stateObj);
-      console.log(result.rows);
+      // console.log(result.rows);
       res.send(result.rows);
     })
     .catch((error) => {
