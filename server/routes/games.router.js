3de8ac029e474a82001d8eb630217184ca73c6b1
JDNafz <nafziger10@gmail.com>
2023-10-08 15:13:44 -0500

    new game button creates a new game with user ID attached

--- server/routes/games.router.js
+++ server/routes/games.router.js
@@ -8,17 +8,18 @@ const { authenticate } = require("passport");
 
 router.get("/current_game", (req, res) => {
   if (req.isAuthenticated()) {
-    console.log("/current_game GET route");
-    console.log("is authenticated?", req.isAuthenticated());
-    console.log("user", req.user);
+    // console.log("/current_game GET route");
+    // console.log("is authenticated?", req.isAuthenticated());
+    // console.log("user id:", req.user.id);
+    const id = req.user.id;
 
     const query = ` SELECT "id", "moves" FROM games 
-                  WHERE user_id = '1'
+                  WHERE user_id = $1
                   LIMIT 1;`;
     pool
-      .query(query,)
+      .query(query,[id])
       .then((result) => {
-        console.log("GOT RESULTS", result.rows[0].moves);
+        // console.log("GOT RESULTS", result.rows[0].moves);
         res.send(result.rows[0].moves);
       })
       .catch((error) => {
@@ -28,8 +29,26 @@ router.get("/current_game", (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  console.log("in post router");
+router.post("/new", (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log('In game/new router \n is authenticated?', req.isAuthenticated());
+    console.log('user', req.user);
+    const id = req.user.id
+    
+    const sqlText = ` INSERT INTO games ("user_id") 
+                      VALUES ($1);`;
+    // Let sql sanitize your inputs (NO Bobby Drop Tables here!)
+    // the $1, $2, etc get substituted with the values from the array below
+    pool
+    .query(sqlText, [id])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+    });
+  }
 });
 
 router.put("/", (req, res) => {
