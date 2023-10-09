06a1e0fb69947df1466ea14e05fbec78ebc4f284
JDNafz <nafziger10@gmail.com>
2023-10-09 17:50:39 -0500

    updated a file name

--- src/components/ChessComponents/GameInfo/GameInfo.jsx
+++ src/components/ChessComponents/GameInfo/GameInfo.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import './GameInfo.css';
+import './gameInfo.css';
 
 export default function GameInfo() {
   // const dispatch = useDispatch();
