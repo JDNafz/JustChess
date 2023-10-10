414d4a12d43bfbb1f210adc38558abc331aa7d11
JDNafz <nafziger10@gmail.com>
2023-10-10 18:53:44 -0500

    profile work in progress

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,87 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
-import './Profile.css';
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useDispatch, useSelector } from "react-redux";
+import "./Profile.css";
+import { useState } from "react";
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("tab1"); // 'tab1' or 'tab2'
+  const [bio, setBio] = useState("Bio");
+  const dispatch = useDispatch();
+  const savedGameList = useSelector((store) => store.savedGameList);
+
+  const handleEditButtonClick = () => {
+    if (isEditing) {
+      dispatch({ type: "SAVE_BIO", payload: {} });
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
+  };
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+    <div className="profile-page">
+      <div className="profile-header">
+        <img
+          id="avatar"
+          src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/st0ckfish/phpwr6GK5.gif"
+          alt="Profile"
+        />
+        <div>
+          <h3>Welcome, {user.username}!</h3>
+          <p>Your ID is: {user.id}</p>
+          <textarea disabled={!isEditing} value={bio}></textarea>
+          <div id="profileButtons">
+            <button onClick={handleEditButtonClick}>
+              {isEditing ? "Save" : "Edit"}
+            </button>
+            <LogOutButton className="btn" />
+          </div>
+        </div>
+      </div>
+      <div className="table">
+        <div className="profile-tabs">
+          <button
+            onClick={() => handleTabChange("tab1")}
+            className={selectedTab === "tab1" ? "active" : ""}
+          >
+            Recent Games
+          </button>
+          <button
+            onClick={() => handleTabChange("tab2")}
+            className={selectedTab === "tab2" ? "active" : ""}
+          >
+            Saved Games
+          </button>
+        </div>
+
+        <div className="profile-content">
+          {selectedTab === "tab1" ? (
+            <div>
+              {savedGameList.map((game, idx) => {
+                return (
+                  <div key={`savedGame${idx}`}>
+                    <li>game</li>
+                    <li> move list: e2e4 </li>
+                  </div>
+                );
+              })}
+              <li>List Item 1</li>
+              <li>List Item 2</li>
+              {/* Add more list items as needed */}
+            </div>
+          ) : (
+            <ul>
+              <li>List Item A</li>
+              <li>List Item B</li>
+              {/* Add more list items as needed */}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
