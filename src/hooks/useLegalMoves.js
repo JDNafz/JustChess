ca1edacf7cf950c526a059d5f7310f62147d695e
JDNafz <nafziger10@gmail.com>
2023-10-13 15:22:48 -0500

    Queen working

--- src/hooks/useLegalMoves.js
+++ src/hooks/useLegalMoves.js
@@ -3,12 +3,14 @@ import { useKnight } from "./useKnight";
 import { usePawn } from "./usePawn";
 import { useRook } from "./useRook";
 import { useBishop } from "./useBishop";
+import { useQueen } from "./useQueen";
 //this function runs when a square is clicked in square.jsx(handleClick)
 export function useLegalMoves() {
   const { getPawnMoves } = usePawn();
   const { getRookMoves } = useRook();
   const { getKnightMoves } = useKnight();
   const { getBishopMoves } = useBishop();
+  const { getQueenMoves } = useQueen();
 
   const getLegalMoves = (square) => {
     // const pieceMoves = {
@@ -33,6 +35,11 @@ export function useLegalMoves() {
       return getKnightMoves(square);
     } else if ( piece === 'b'){
       return getBishopMoves(square);
+    } else if ( piece === 'q') {
+      return getQueenMoves(square);
+      // or skip the file and use the following:
+      return [...getRookMoves(square),...getBishopMoves(square)]
+
     }
     return getPawnMoves(square)
     
--- src/hooks/useQueen.js
+++ src/hooks/useQueen.js
@@ -1,27 +1,56 @@
+// import { useSelector } from "react-redux";
+import { useRook } from "./useRook";
+import { useBishop } from "./useBishop";
 
-const makeAllMoves = require("../../server/modules/makeAllMoves");
-const board = makeAllMoves(["a2e5","e2d2","e1b1"]);
+export function useQueen(){
+  const { getRookMoves } = useRook();
+  const { getBishopMoves } = useBishop();
 
-  function getQueenMoves(selectedPiece){
-    // console.log("Searching for valid Knight Moves");
-
-
-
-    console.log("valid moves:", validMoves.length)
-    return validMoves;
+  function getQueenMoves(sP){
+    return [...getRookMoves(sP),...getBishopMoves(sP)]
   };
-//   return { getKnightMoves };
-// }
 
-const example = {
-  id: 36,
-  coordinate: "e5",
-  x: 4,
-  y: 4,
-  piece: "wb",
-  underAttackFromWhite: false,
-  underAttackFromBlack: false,
-  isBlack: false,
-};
+  return { getQueenMoves };
+}
 
-getQueenMoves(example);
+// const example = {
+//   id: 36,
+//   coordinate: "e5",
+//   x: 4,
+//   y: 4,
+//   piece: "wb",
+//   underAttackFromWhite: false,
+//   underAttackFromBlack: false,
+//   isBlack: false,
+// };
+
+// getQueenMoves(example);
+
+
+
+// const makeAllMoves = require("../../server/modules/makeAllMoves");
+// const board = makeAllMoves(["a2e5","e2d2","e1b1"]);
+
+//   function getQueenMoves(selectedPiece){
+//     // console.log("Searching for valid Knight Moves");
+
+
+
+//     console.log("valid moves:", validMoves.length)
+//     return validMoves;
+//   };
+// //   return { getKnightMoves };
+// // }
+
+// const example = {
+//   id: 36,
+//   coordinate: "e5",
+//   x: 4,
+//   y: 4,
+//   piece: "wb",
+//   underAttackFromWhite: false,
+//   underAttackFromBlack: false,
+//   isBlack: false,
+// };
+
+// getQueenMoves(example);
