e089a4f1fd92db59d4d882657544905815ffdd0a
JDNafz <nafziger10@gmail.com>
2023-10-14 15:56:31 -0500

    made a mess, gunna go restart with other code

--- src/hooks/usePawn.js
+++ src/hooks/usePawn.js
@@ -1,38 +1,29 @@
 import { useSelector } from "react-redux";
-// no default to name the export and force clarity in other areas of project
+import checkVector from "./checkVector";
 
 export function usePawn() {
   const board = useSelector((store) => store.board);
 
+  //sP for selectedPiece
   const getPawnMoves = (sP) => {
-    const sPColor = sP.piece[0];
-    const isStartingRow =
-      // white starts in row 2 (indexed at 1)
-      (sPColor === "w" && sP.y === 1) ||
-      //black starts in row 7(indexed at 6)
-      (sPColor === "b" && sP.y === 6);
+    const sPIsWhite = sP.piece[0] === "w" ? true : false
+    const isStartingRow = sPIsWhite ? sP.y === 1: sP.y ===6;
 
-    //if piece is white next move to sq to examine is y + 1
-    // if black => y - 1
-    const pawnMoves = board.filter((sq) => {
-      const y = sPColor === "w" ? sP.y + 1 : sP.y - 1;
-      const y2 = sPColor === "w" ? sP.y + 2 : sP.y - 2;
-      const firstSquare = sq.x === sP.x && sq.y === y;
-      const secondSquare = sq.x === sP.x && sq.y === y2;
-      return firstSquare || secondSquare;
-    });
-    if (pawnMoves[0].piece !== null) {
-      // return validMoves;
-      // }
+    if (isStartingRow){
+
+      const vector = [];
+      x = sP.x - 1;
+    y = sP.y - 1;
+    while (x >= 0 && y >= 0) {
+      board.map((sq) => {
+        if (sq.x === x && sq.y === y) {
+          vector4.push(sq);
+        }
+      });
+      x--;
+      y--;
     }
+  }
   };
   return { getPawnMoves };
 }
-// coordinate: "d2";
-// id: 12;
-// isBlack: false;
-// piece: "wp";
-// underAttackFromBlack: false;
-// underAttackFromWhite: false;
-// x: 3;
-// y: 1;
