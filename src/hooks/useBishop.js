edc7c4574a5451ac97c7b056a5451454d96fd320
JDNafz <nafziger10@gmail.com>
2023-10-14 15:48:41 -0500

    moved checkVector into it's own file

--- src/hooks/useBishop.js
+++ src/hooks/useBishop.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import checkVector from "./checkVector"
 
 export function useBishop() {
   const board = useSelector((store) => store.board);
@@ -65,26 +66,6 @@ export function useBishop() {
     }
 
     const pieceColor = sP.piece[0];
-
-    function checkVector(vector) {
-      let result = [];
-      // console.log(vector.length)
-      for (let i = 0; i < vector.length; i++) {
-        //if sq is empty add to valid moves
-        // console.log(vector[i]);
-        if (vector[i].piece === null) {
-          result.push(vector[i]);
-        } else {
-          //if piece is a different color add it to valid moves
-          if (pieceColor !== vector[i].piece[0]) {
-            result.push(vector[i]);
-            // console.log("I hit a piece i'll add it")
-          }
-          break; //a piece has been found, stop looking, rook can't jump over other pieces.
-        }
-      }
-      return result;
-    }
 
     // console.log("v4:",vector4);
     const validMoves = [];
--- src/hooks/usePawn.js
+++ src/hooks/usePawn.js
@@ -1,49 +1,33 @@
 import { useSelector } from "react-redux";
 // no default to name the export and force clarity in other areas of project
 
-
-
 export function usePawn() {
   const board = useSelector((store) => store.board);
-  
-  const getPawnMoves = (selectedPiece) => {
-    
-    const pieceColor = selectedPiece.piece[0];
+
+  const getPawnMoves = (sP) => {
+    const sPColor = sP.piece[0];
     const isStartingRow =
       // white starts in row 2 (indexed at 1)
-      (pieceColor === "w" && selectedPiece.y === 1) ||
+      (sPColor === "w" && sP.y === 1) ||
       //black starts in row 7(indexed at 6)
-      (pieceColor === "b" && selectedPiece.y === 6);
+      (sPColor === "b" && sP.y === 6);
 
     //if piece is white next move to sq to examine is y + 1
     // if black => y - 1
-    const nextMoveYOne =
-      pieceColor === "w" ? selectedPiece.y + 1 : selectedPiece.y - 1;
-    //or + 2 or - 2
-    const nextMoveYTwo = isStartingRow
-      ? pieceColor === "w"
-        ? selectedPiece.y + 2
-        : selectedPiece.y - 2
-      : null;
-
-    const nextRowSquares = board.filter((square) => {
-      const isOnSameX = square.x === selectedPiece.x;
-      const isValidOneSquareMove =
-        square.x === selectedPiece.x && square.y === nextMoveYOne;
-      const isValidTwoSquareMove =
-        nextMoveYTwo && isOnSameX && square.y === nextMoveYTwo;
-      const squareIsNotOccupied = !square.piece;
-      return (
-        (isValidOneSquareMove || isValidTwoSquareMove) && squareIsNotOccupied
-      );
+    const pawnMoves = board.filter((sq) => {
+      const y = sPColor === "w" ? sP.y + 1 : sP.y - 1;
+      const y2 = sPColor === "w" ? sP.y + 2 : sP.y - 2;
+      const firstSquare = sq.x === sP.x && sq.y === y;
+      const secondSquare = sq.x === sP.x && sq.y === y2;
+      return firstSquare || secondSquare;
     });
-
-    return nextRowSquares;
+    if (pawnMoves[0].piece !== null) {
+      // return validMoves;
+      // }
+    }
   };
-
   return { getPawnMoves };
 }
-
 // coordinate: "d2";
 // id: 12;
 // isBlack: false;
