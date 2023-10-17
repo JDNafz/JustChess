f83d36641b8e1d606a7bfdcca4aebbdaea420b78
JDNafz <nafziger10@gmail.com>
2023-10-17 11:46:24 -0500

    king in progress

--- src/hooks/useKing.js
+++ src/hooks/useKing.js
@@ -6,23 +6,49 @@ const board = makeAllMoves([
 function getKingMoves(sP) {
   // console.log("Searching for valid Knight Moves");
   const kingColor = sP.piece[0];
-  getAllAttacks(board, kingColor);
+  // getAllAttacks(board, kingColor);
 
+  const king8 = [
+    [sP.x - 1, sP.y - 1],
+    [sP.x - 1, sP.y    ],
+    [sP.x - 1, sP.y + 1],
+    [sP.x    , sP.y + 1],
+    [sP.x + 1, sP.y + 1],
+    [sP.x + 1, sP.y    ],
+    [sP.x + 1, sP.y - 1],
+    [sP.x    , sP.y - 1]
+  ]
+  const king8Squares = board.filter((sq)=> {
+    for (let coordinates of king8){
+      if (coordinates[0] === sq.x && coordinates[1] === sq.y){
+        return sq
+      }
+    }
+  })
+    
+
+
+  
+  const validMoves = king8Squares
   console.log("valid moves:", validMoves.length);
   return validMoves;
 }
 //   return { getKnightMoves };
 // }
 
-const example = {
+
+
+
+const selectedPiece = {
   id: 36,
-  coordinate: "e5",
+  coordinate: "e4",
   x: 4,
-  y: 4,
+  y: 3,
   piece: "wk",
   underAttackFromWhite: false,
   underAttackFromBlack: false,
   isBlack: false,
 };
 
-getKingMoves(example);
+const output = getKingMoves(selectedPiece);
+console.log(output)
