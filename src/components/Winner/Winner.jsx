148a222ef622a18ea1fd0beae38608c634447925
JDNafz <nafziger10@gmail.com>
2023-10-19 18:05:41 -0500

    winner recognized upon refresh

--- src/components/Winner/Winner.jsx
+++ src/components/Winner/Winner.jsx
@@ -1,13 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import "./Winner.css";
+import { useEffect } from "react";
 
 export default function Menu() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const board = useSelector((store) => store.board);
+ 
+  useEffect(() => {
+  const kings = board.filter((sq) => {
+    if (sq.piece !== null) {
+      if (sq.piece[1] === "k") {
+        return sq;
+      }
+    }
+  });
+  if (kings.length === 1) {
+    dispatch({
+      type: "SET_WINNER",
+      payload: { bool: true, color: kings[0].piece[0] },
+    });
+  }
+  }, [board]);
+
+  // dispatch({ type: "SET_WINNER", payload: 'b'})
   const winner = useSelector((store) => store.winner);
 
   // const inputsText = showInputs ? "Hide Inputs" : "Show Inputs";
-  const winnerText = winner.color === "w" ? "White wins!!!": "Black wins!!!";
+  const winnerText = winner.color === "w" ? "White wins!!!" : "Black wins!!!";
 
   return <>{winner.bool && <div className="winner">{winnerText}</div>}</>;
 }
