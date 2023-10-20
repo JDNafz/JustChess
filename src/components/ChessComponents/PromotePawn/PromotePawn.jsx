35054146eca54ca7defa68cd3ab0cd9f7b0c05a1
JDNafz <nafziger10@gmail.com>
2023-10-20 11:57:06 -0500

    formatted white selection for promotion

--- src/components/ChessComponents/PromotePawn/PromotePawn.jsx
+++ src/components/ChessComponents/PromotePawn/PromotePawn.jsx
@@ -1,14 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import Image from "../Image/Image";
 
-import './PromotePawn.css';
+import "./PromotePawn.css";
 
 import { useEffect } from "react";
 import { useState } from "react";
 
 export default function PromotePawn() {
   const dispatch = useDispatch();
-  const promotion = useSelector((store) => store.promotion)
+  const promotion = useSelector((store) => store.promotion);
   const board = useSelector((store) => store.board);
 
   useEffect(() => {
@@ -24,20 +24,39 @@ export default function PromotePawn() {
       }
     });
     if (pawnArrayOfOne.length === 1) {
-      dispatch({type: "SET_PROMOTION_TRUE"})
+      dispatch({ type: "SET_PROMOTION_TRUE" });
     }
   }, [board]);
 
+  const selectPromotion = (piece) => {
+    // replace last move with "e8Q"
+    //special move replace pawn square with selected piece
+    //makeSpecialMove()
+  };
   return (
     <>
       {promotion && (
-        <div className="selectNewPiece">
-          
-          <Image piece={"wq"} />
-          <Image piece={"wr"} />
-          <Image piece={"wb"} />
-          <Image piece={"wnr"} />
-
+        <div className="newPieceContainer">
+          <div
+            className="selectwq selectPiece"
+            onClick={() => selectPromotion("wq")}
+          >
+            <Image piece={"wq"} />
+          </div>
+          <div className="secondaryOptions">
+            <div
+              className="selectwr selectPiece"
+              onClick={() => selectPromotion("wr")}
+            >
+              <Image piece={"wr"} />
+            </div>
+            <div className="selectPiece" onClick={() => selectPromotion("wb")}>
+              <Image piece={"wb"} />
+            </div>
+            <div className="selectPiece" onClick={() => selectPromotion("wnr")}>
+              <Image piece={"wnr"} />
+            </div>
+          </div>
         </div>
       )}
     </>
