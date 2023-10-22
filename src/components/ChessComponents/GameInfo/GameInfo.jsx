bdc5ae73ff8fd0b8643503a753cca4516889d34b
JDNafz <nafziger10@gmail.com>
2023-10-22 15:58:22 -0500

    scroll added to moveList

--- src/components/ChessComponents/GameInfo/GameInfo.jsx
+++ src/components/ChessComponents/GameInfo/GameInfo.jsx
@@ -1,5 +1,5 @@
-import { useSelector } from 'react-redux';
-import './GameInfoTheUnCursed.css'
+import { useSelector } from "react-redux";
+import "./GameInfoTheUnCursed.css";
 
 export default function GameInfo() {
   const gameLog = useSelector((store) => store.gameLog);
@@ -10,42 +10,39 @@ export default function GameInfo() {
     if (idx === moves.length - 1 && moves.length % 2 === 1) {
       return (
         <tr key={`move${idx}`}>
-          <td>{idx/2 + 1}</td>
+          <td>{idx / 2 + 1}</td>
           <td>{move}</td>
-          <td>  </td>
+          <td> </td>
         </tr>
-
       );
     } else {
       if (idx % 2 === 0) {
         return (
           <tr key={`move${idx}`}>
-            <td>{idx/2 + 1}</td>
+            <td>{idx / 2 + 1}</td>
             <td>{move}</td>
             <td>{moves[idx + 1]}</td>
           </tr>
         );
       }
     }
-  })
-
-
+  });
 
   return (
-    <div id="gameInfo">
-      <p>Currently only working when logged in*</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Turn</th>
-            <th> </th>
-            <th> </th>
-          </tr>
-        </thead>
-        <tbody>
-        {tableContent}
-        </tbody>
-      </table>
+    <div className="moveList">
+      <p>Only working when logged in*</p>
+      <div className="gameInfo">
+        <table>
+          <thead>
+            <tr>
+              <th>Turn</th>
+              <th> </th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>{tableContent}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
