ba314a2d379b227b678f0ffc4cdfb94ceffa3d36
JDNafz <nafziger10@gmail.com>
2023-10-22 23:02:12 -0500

    fixed issues loggin into a new game
    and not having a new game upon registration

--- src/redux/sagas/login.saga.js
+++ src/redux/sagas/login.saga.js
@@ -21,7 +21,6 @@ function* loginUser(action) {
     // after the user has logged in
     // get the user information from the server
     yield put({ type: 'FETCH_USER' });
-    yield put({ type: "NEW_GAME"})
 
   } catch (error) {
     console.log('Error with user login:', error);
--- src/redux/sagas/registration.saga.js
+++ src/redux/sagas/registration.saga.js
@@ -1,29 +1,36 @@
-import { put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
 
 // worker Saga: will be fired on "REGISTER" actions
 function* registerUser(action) {
   try {
     // clear any existing error on the registration page
-    yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
+    yield put({ type: "CLEAR_REGISTRATION_ERROR" });
 
     // passes the username and password from the payload to the server
-    yield axios.post('/api/user/register', action.payload);
+    yield axios.post("/api/user/register", action.payload);
 
-    // automatically log a user in after registration
-    yield put({ type: 'LOGIN', payload: action.payload });
-
-    // set to 'login' mode so they see the login screen
-    // after registration or after they log out
-    yield put({ type: 'SET_TO_LOGIN_MODE' });
+    // Manually log a user in after registration Then create new game
+    yield put({ type: "CLEAR_LOGIN_ERROR" });
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    yield axios.post("/api/user/login", action.payload, config);
+    yield put({ type: "FETCH_USER" });
+    yield put({ type: "NEW_GAME" });
+    //doing the initial log in here rather than the "LOGIN" call 
+    //allows for users registering to establish their first "NEW GAME" 
+    //and previous players to not abandon their previous game.
+    
   } catch (error) {
-    console.log('Error with user registration:', error);
-    yield put({ type: 'REGISTRATION_FAILED' });
+    console.log("Error with user registration:", error);
+    yield put({ type: "REGISTRATION_FAILED" });
   }
 }
 
 function* registrationSaga() {
-  yield takeLatest('REGISTER', registerUser);
+  yield takeLatest("REGISTER", registerUser);
 }
 
 export default registrationSaga;
